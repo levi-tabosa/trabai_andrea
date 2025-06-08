43f6b3b1da2831c3c0af06cc4c83f9a3d1372ff3
Natan Freitas <natanael_freitas@outlook.com>
2025-06-08 12:42:33 -0300

    Create game_over.gd

---
+++ scripts/game_over.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/cutscene_2.tscn")
+
+
+func _on_controls_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_sair_pressed() -> void:
+	pass # Replace with function body.
