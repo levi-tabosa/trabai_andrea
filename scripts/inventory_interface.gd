e39291b1c259a844a7c939cb9cee0450944a5842
Levi Tabosa <silvacostaelevi@gmail.com>
2025-06-10 15:33:53 -0300

    inventario e outras fita novamente

--- scripts/inventory_interface.gd
+++ scripts/inventory_interface.gd
@@ -5,7 +5,7 @@ var grabbed_slot_data: SlotData
 @onready var player_inventory: PanelContainer = $HBoxContainer/PlayerInventory
 @onready var grabbed_slot: PanelContainer = $HBoxContainer/Slot
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:	
 	if grabbed_slot.visible:
 		grabbed_slot.global_position = get_global_mouse_position() + Vector2(4, 4)
 
@@ -16,17 +16,13 @@ func set_player_inventory_data(inventory_data: InventoryData) -> void:
 func on_inventory_interact(inventory_data: InventoryData, index: int, button_index: int) -> void:
 	match [grabbed_slot_data, button_index]:
 		[null, MOUSE_BUTTON_LEFT]:
-			# Handle left-click interaction
 			grabbed_slot_data = inventory_data.grab_slot_data(index)
 			print("Grabbed slot data:", grabbed_slot_data)
 		[null, MOUSE_BUTTON_RIGHT]:
-			# Handle right-click interaction
 			print("Right-click on slot index:", index)
 		[_, MOUSE_BUTTON_LEFT]:
 			grabbed_slot_data = inventory_data.drop_slot_data(grabbed_slot_data, index)
 			print(grabbed_slot_data == null)
-	# Handle inventory interaction here
-	print("Inventory interact:", inventory_data, "Index:", index, "Button index:", button_index)
 	update_grabbed_slot()
 
 func update_grabbed_slot() -> void:
